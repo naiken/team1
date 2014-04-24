51773f9aa657a1cbfc457e94ac5a902ab29ef8cd
谷尾 和是 <taniomasashi@tanio.local>
2014-04-24 07:51:16 +0700

    masashi

---
+++ example(moleAlarm_free)/HowToViewController.h
@@ -0,0 +1,13 @@
+//
+//  HowToViewController.h
+//  example
+//
+//  Created by 谷尾 和是 on 2014/03/14.
+//  Copyright (c) 2014年 Masa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+@interface HowToViewController : UIViewController
+- (IBAction)done:(id)sender;
+@end
---
+++ example(moleAlarm_free)/example/MoleViewController.h
@@ -0,0 +1,15 @@
+//
+//  MoleViewController.h
+//  example
+//
+//  Created by 谷尾 和是 on 2014/04/07.
+//  Copyright (c) 2014年 Masa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+#import "FirstViewController.h"
+
+@interface MoleViewController : UIViewController
+
+@end
